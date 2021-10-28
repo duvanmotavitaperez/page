f67d35d03191adcd7c274edeb592b22291d42bae
Duvan200207 <navud20@gmail.com>
2021-10-28 04:59:34 -0500

    includes dashboard

--- form.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.fields.html5 import EmailField
-from wtforms.validators import DataRequired
-
-class Fields(FlaskForm):
-    user = StringField('user', validators=[DataRequired(message='Por favor completa éste campo')])
-    password = PasswordField()
